0a600963f2489b9fdf12a4264028f3e0dc744af4
Oleg <oleg.baybekov@gmail.com>
2023-03-22 15:51:47 +0200

    first commit

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import error from './error.png'
+
+
+const ErrorMessage = () => {
+    return (
+        <img style={{width: '120px', margin: '0 auto'}} src={error} alt="Error" />
+    )
+}
+
+export default ErrorMessage;
